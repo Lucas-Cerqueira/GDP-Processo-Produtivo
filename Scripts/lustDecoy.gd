979348ac4e8df5bbcdc9f9aa7913c37d493c9a49
Lucas <lucas.cerqueira@poli.ufrj.br>
2017-05-23 21:38:40 -0300

    Fixed small warning when exploding decoy

--- Scripts/lustDecoy.gd
+++ Scripts/lustDecoy.gd
@@ -6,8 +6,10 @@ func _ready():
 func Explode():
 	var baseDamage = get_node("/root/GlobalVariables").lustDecoyExplosionDamage
 	var array = get_overlapping_bodies()
+	set_enable_monitoring(false)
 	if (array.size() != 0):
 		for enemy in array:
-			enemy.TakeHit (baseDamage)
+			if (enemy.has_method("TakeHit")):
+				enemy.TakeHit (baseDamage)
 	
 	queue_free()
